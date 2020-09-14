6f8171e9ae7175a9ce477ea509d4ffe5e9abe283
Noah Troy <Admin@Priveasy.org>
2020-09-13 21:25:52 -0400

    Added noise and adjusted values

--- BotBlock.py
+++ BotBlock.py
@@ -11,10 +11,11 @@ height = 250
 
 charset = 'abcdefghijkmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ23456789'
 
-fonts = ['fonts/Ballbase.ttf' , 'fonts/GiantRobotArmy-Medium.ttf' , 'fonts/Manustype.ttf' , 'fonts/NANOTYPE.ttf' , 'fonts/PixelGrunge.ttf' , 'fonts/Reappeat.ttf' , 'fonts/Savior1.ttf' , 'fonts/Thruster-Regular.ttf' , 'fonts/To The Point.ttf']
+fonts = ['fonts/Ballbase.ttf' , 'fonts/GiantRobotArmy-Medium.ttf' , 'fonts/Manustype.ttf' , 'fonts/NANOTYPE.ttf' , 'fonts/PixelGrunge.ttf' ,  'fonts/Savior1.ttf' , 'fonts/Thruster-Regular.ttf' , 'fonts/To The Point.ttf']
 
 borderBufferPercentage = 12
 warpLimitPercentage = 10
+maxNoise = 10
 
 def getTextAndAtts():
 	length = textLength
@@ -42,7 +43,7 @@ def getTextAndAtts():
 		charList.append(randint(minFontSize , maxFontSize))
 
 		if ((i == (length - 1)) or (i == 0)):
-			hPosition = randint(((sectorSize * i) + sectorBuffer) , ((sectorSize * (i + 1)) - sectorBuffer))
+			hPosition = randint(((sectorSize * i) + sectorBuffer) , ((sectorSize * (i + 1)) - (sectorBuffer * 2)))
 		else:
 			hPosition = randint((sectorSize * i) , (sectorSize * (i + 1)))
 
@@ -65,17 +66,27 @@ def generate():
 	captcha = Image.new('RGB' , (width , height) , (r , g , b))
 	d = ImageDraw.Draw(captcha)
 
-	# Change color shade before adding text/noise:
-	multValue = choice([(randint(0 , 90) / 100) , (randint(110 , 200) / 100)])
-
+	# Add color shades before adding text/noise:
+	multValue = choice([(randint(0 , 80) / 100) , (randint(120 , 200) / 100)])
 	nr = (int(r * multValue) % 255)
 	ng = (int(g * multValue) % 255)
 	nb = (int(b * multValue) % 255)
 
+	multValue = choice([(randint(0 , 80) / 100) , (randint(120 , 200) / 100)])
+	snr = (int(r * multValue) % 255)
+	sng = (int(g * multValue) % 255)
+	snb = (int(b * multValue) % 255)
+
 	# Add the captcha text:
 	for charAndAtts in getTextAndAtts():
 		f = ImageFont.truetype(choice(fonts) , charAndAtts[1])
-		d.text((charAndAtts[2] , charAndAtts[3]) , charAndAtts[0] , font = f , fill = (nr , ng , nb))
+		d.text((charAndAtts[2] , charAndAtts[3]) , charAndAtts[0] , font = f , fill = choice([(nr , ng , nb) , (snr , sng , snb)]))
+
+	# Add noise:
+	for i in range(randint(0 , maxNoise)):
+		startX = randint(0 , width)
+		startY = randint(0 , height)
+		d.arc([(startX , startY) , (randint(startX , width) , randint(startY , height))] , randint(0 , 359) , randint(0 , 359) , width = randint(1 , 3) , fill = choice([(nr , ng , nb) , (snr , sng , snb)]))
 
 	# Warp the image:
 	widthLimit = int(width * (warpLimitPercentage / 100))
